8870c1bfaf33230540d1bfc3c61f4c0ae9e31d4d
qisu1991 <72512628+qisu1991@users.noreply.github.com>
2023-06-10 19:15:02 +0800

    Update DynamicNetworks.jl

--- DynamicNetworks.jl
+++ DynamicNetworks.jl
@@ -1,7 +1,7 @@
-# function to obtain reproductive value of each individual, i.e. pi_{i}^{beta} (see Equation 4 in the main text)
+# function to obtain reproductive value of each individual, i.e. pi_{i}^{beta} (see Equation 5 in the main text)
 # input -----
 # edge_seq: edge list 
-# transition: network transition matrix 
+# transition: network transition list  
 # output -----
 # an N-by-L matrix, where the element in the row i and column beta is pi_{i}^{beta}
 function pi_DB(edge_seq::Array{Float64,2}, transition::Array{Float64,2})
@@ -9,6 +9,7 @@ function pi_DB(edge_seq::Array{Float64,2}, transition::Array{Float64,2})
     N = Int64(maximum(edge_seq[:,1:2]))
     # number of networks
     L = Int64(maximum(edge_seq[:,4]))
+    # an LN-by-N matrix, where the element in the row (beta-1)*N+i and column j is p_ji^{beta}
     P = spzeros(Float64, L*N, N)
     for i = 1:L
         edge_list_i = findall(isequal(i), edge_seq[:,4])
@@ -16,7 +17,9 @@ function pi_DB(edge_seq::Array{Float64,2}, transition::Array{Float64,2})
         deg = sum(M_i, dims = 1)
         P[(i-1)*N+1:i*N,:] = M_i./deg
     end
+    # transition matrix between L networks
     Q = sparse(transition[:,1], transition[:,2], transition[:,3], L, L)
+
     MatA = spzeros(Float64, L*N, L*N)
     for i = 1:L
         for j = 1:L
@@ -61,7 +64,7 @@ function pi_DB(edge_seq::Array{Float64,2}, transition::Array{Float64,2})
     return reshape(pi_solution,:,L)
 end
 
-# function to obtain eta_ij (see Equation 5 in the main text)
+# function to obtain eta_ij (see Equation 6 in the main text)
 # input -----
 # edge_seq: edge list 
 # transition: network transition matrix 
@@ -141,7 +144,7 @@ function eta_DB(edge_seq::Array{Float64,2}, transition::Array{Float64,2})
     return transpose(eta_solution)
 end
 
-# function to obtain u0, v0, u2, v2, and the critical benefit-to-cost ratio (see Equations 10 and 11 in the main text)
+# function to obtain u0, v0, u2, v2, and the critical benefit-to-cost ratio (see Equations 11 and 12 in the main text)
 # input -----
 # edge_seq: edge list 
 # transition: network transition matrix 
@@ -184,27 +187,77 @@ function bc_DB(edge_seq::Array{Float64,2}, transition::Array{Float64,2})
     return (v0-v2)/(u0-u2)
 end
 
+# generate a two-subgraph network like Figure 2a
+# input ----
+# N: network size 
+# alpha: the node fraction of the top subgraph
+# output ----
+# the edge list 
+# elements in each row: [the source node id, the target node id, the edge weight, the network id]
+function edge_list(N::Int64, alpha::Float64)
+    N1 = Int64(floor(N*alpha))
+    N2 = N-N1 
+    # generate Network 1 
+    edge_seq = zeros(Int64,N1^2+N1+N2^2+N2,4) 
+    # star subgraph
+    id = 1
+    for i = 1:N1-1
+        edge_seq[id, :] = [N1 i 1 1]
+        edge_seq[id+1, :] = [i N1 1 1]
+        id = id+2
+    end
+    # complete subgraph 
+    for i = N1+1:N-1
+        for j = i+1:N
+            edge_seq[id, :] = [i j 1 1]
+            edge_seq[id+1, :] = [j i 1 1]
+            id = id+2
+        end
+    end
+    edge_seq[id, :] = [N1 N 1 1]
+    edge_seq[id+1, :] = [N N1 1 1]
+    id = id+2
+
+    # generate Network 2 
+    # complete subgraph
+    for i = 1:N1-1
+        for j = i+1:N1
+            edge_seq[id, :] = [i j 1 2]
+            edge_seq[id+1, :] = [j i 1 2]
+            id = id+2
+        end
+    end
+    for i = N1+1:N-1 
+        edge_seq[id, :] = [N i 1 2]
+        edge_seq[id+1, :] = [i N 1 2]
+        id = id+2
+    end
+    edge_seq[id, :] = [N1 N 1 2]
+    edge_seq[id+1, :] = [N N1 1 2]
+
+    return edge_seq
+end
+
 
 # main function 
-# calculate the benefit-to-cost ratio (b/c)^* for the two-clique network in Figure 2a
-################################################################################
-#---
 using Printf
 using Statistics
 using LinearAlgebra
 using DelimitedFiles
-using TickTock
 using IterativeSolvers
 using SparseArrays
-# rng = MersenneTwister(1234)
 
-# input the edge list of a two-clique network with size N = 40 and a = 0.5
+# Figure 2a
+# calculate the benefit-to-cost ratio (b/c)^* for the two-subgraph network in Figure 2
+################################################################################
+# generate a two-subgraph network with size N = 40 and a = 0.5
 # elements in each row: [the source node id, the target node id, the edge weight, the network id]
-edge_seq = readdlm("edge_list1", '\t', Int64)
+N = 40
+alpha = 0.5
+edge_seq = edge_list(N, alpha)
 
 # set the transition pattern
-# elements in each row: [the current network id, the next network id, the transition probability]
-N = 40
+# network transition list, elements in each row: [the current network id, the next network id, the transition probability]
 transition = [1 1 1-1/N;
               1 2 1/N;
               2 1 1/N;
@@ -212,9 +265,43 @@ transition = [1 1 1-1/N;
 
 # calculate the critical benefit-to-cost ratio
 bc = bc_DB(1.0*edge_seq, transition)
-println("For the two-clique network with N = 40 and a = 0.5, the critical benefit-to-cost ratio (b/c)* is ", bc)
+println("For the two-subgraph network with N = 40 and a = 0.5, the critical benefit-to-cost ratio (b/c)* is ", bc)
 
-# input the edge list of a two-clique network with size N = 40 and a = 0.7
-edge_seq = readdlm("edge_list2", '\t', Int64)
+# generate a two-subgraph network with size N = 40 and a = 0.7
+alpha = 0.7
+edge_seq = edge_list(N, alpha)
 bc = bc_DB(1.0*edge_seq, transition)
 println("For the two-clique network with N = 40 and a = 0.7, the critical benefit-to-cost ratio (b/c)* is ", bc)
+
+
+# Figure 3a
+# calculate the benefit-to-cost ratio (b/c)^* for the two-subgraph network in the inset of Figure 3a
+################################################################################
+# consider a range of network sizes
+N_list = 6:2:24 
+alpha = 0.5 
+
+# dynamic networks
+bc_list = zeros(Float64,length(N_list))
+for i = 1:length(N_list)
+    edge_seq = edge_list(N_list[i],alpha)
+    transition = [1 1 1-1/N_list[i];
+                  1 2 1/N_list[i];
+                  2 1 1/N_list[i];
+                  2 2 1-1/N_list[i]]
+    bc_list[i] = bc_DB(1.0*edge_seq, transition)
+end
+println("For the two-subgraph dynamic networks, the critical benefit-to-cost ratios (b/c)* are ", bc_list)
+
+
+# static networks
+bc_list = zeros(Float64,length(N_list))
+for i = 1:length(N_list)
+    edge_seq = edge_list(N_list[i],alpha)
+    id = findall(isequal(1), edge_seq[:,4])
+    # Static network 1
+    edge_seq_net1 = edge_seq[id, :]
+    transition = [1 1 1.0]
+    bc_list[i] = bc_DB(1.0*edge_seq_net1, transition)
+end
+println("For the two-subgraph static networks, the critical benefit-to-cost ratios (b/c)* are ", bc_list)
